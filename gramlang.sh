f4e860ba7b0baa47b357d1d743b00dee4fd7d14d
Aarik Pokras <85502246+aarikpokras@users.noreply.github.com>
2023-12-07 12:05:46 -0500

    Add files via upload

---
+++ gramlang.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo Please pass a file name.
+else
+  if [ -f "$1" ]; then
+    while IFS= read -r -u 3 i; do
+      if [[ "$i" =~ "say without new line" ]]; then
+        printf "$i" | sed 's/say without new line //'
+      elif [[ "$i" == "@@"* ]]; then
+        :
+      elif [[ "$i" =~ "pause for" ]]; then
+        duration=$(echo $i | sed -e 's/pause for //' -e 's/seconds//')
+        sleep $duration
+      elif [[ "$i" =~ "say" ]]; then
+        echo $i | sed 's/say //'
+      elif [[ "$i" == "get machine hardware name" ]]; then
+        uname -m
+      elif [[ "$i" == "get machine architecture" ]]; then
+        uname -p
+      elif [[ "$i" =~ "read" ]]; then
+        if [ -f "$(echo $i | sed 's/read //')" ]; then
+          cat $(echo $i | sed 's/read //')
+        else
+          echo gramlang: read: no such file $(echo $i | sed 's/read //')
+        fi
+# Make variable setter.
+      elif [[ "$i" =~ "user input" ]]; then
+        if [[ "$(echo $i | sed 's/user input //')" =~ "with prompt" ]]; then
+          printf "$(echo $i | sed 's/user input with prompt //')"
+          read -r var1
+          echo $var1 >> gram.out
+        else
+          echo gramlang: user input: invalid syntax. Please include \'with prompt\'.
+        fi
+      fi
+    done 3< "$1"
+  else
+    echo gramlang: $1: No such file
+  fi
+fi
